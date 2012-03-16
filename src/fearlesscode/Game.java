9253181dbb46ffeeb2a4721c8d0d263bc3594bfa
Bence Danyi <madbence@gmail.com>
2012-03-16 11:39:27 +0100

    Game osztaly implementacioja elkezdve

--- src/fearlesscode/Game.java
+++ src/fearlesscode/Game.java
@@ -4,17 +4,40 @@ public class Game {
 
 	private PlayField playField;
 
-	public Game(){
-
+	/**
+	 * Letrehoz egy ugy Game peldanyt
+	 *
+	 * A Game objektum felelos a palyak betolteseert, valamint a fobb jatekmozzanatok kezeleseert
+	 */
+	public Game()
+	{
+		Logger.reg(this, "game");
 	}
 
 
-	public void loadNextLevel(){
+	/**
+	 * Betolti a kovetkezo palyat
+	 *
+	 * Az aktualis palya ismeretében megkeresi a kovetkezo palya specifikaciojat,
+	 * es legyartatja a PlayFieldBuilder osztallyal.
+	 */
+	public void loadNextLevel()
+	{
+		Logger.call(this, "loadNextLevel()");
 
+		Logger.ret(this, "loadNextLevel()");
 	}
 
-	public void start(){
+	/**
+	 * Elinditja a jatekot
+	 * 
+	 * Betolti a megfelelo specifikacioval rendelkezo palyat, majd elinditja a jatekot vele.
+	 */
+	public void start()
+	{
+		Logger.call(this, "start()");
 
+		Logger.ret(this, "start()");
 	}
 
 }
