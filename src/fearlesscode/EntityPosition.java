3a8b0caf6c1569cab784c25526b1753c5437c9dd
Bence Danyi <madbence@gmail.com>
2012-03-18 22:06:30 +0100

    EntityPosition fix

--- src/fearlesscode/EntityPosition.java
+++ src/fearlesscode/EntityPosition.java
@@ -1,9 +1,6 @@
-/**
- * Created by IntelliJ IDEA.
- * User: tibikolozsi
- * Date: 3/16/12
- * Time: 12:48 PM
- * To change this template use File | Settings | File Templates.
- */
-public class EntityPosition extends Position{
+package fearlesscode;
+
+public class EntityPosition extends Position
+{
+	
 }
