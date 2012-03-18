09c2475bb84ce2ad4717d5b13a8f89d377be3730
Bence Danyi <madbence@gmail.com>
2012-03-18 22:13:59 +0100

    SpawnPoint fix

--- src/fearlesscode/SpawnPoint.java
+++ src/fearlesscode/SpawnPoint.java
@@ -4,13 +4,13 @@ public class SpawnPoint extends Entity
 {
 	public SpawnPoint(PlayField playField)
 	{
-		Logger.call(this,"SpawnPoint.init()");
+		//Logger.call(this,"SpawnPoint.init()");
 		super(playField);
-		Logger.ret(this,"SpawnPoint.init()");
+		//Logger.ret(this,"SpawnPoint.init()");
 	}
 	public void meetPlayer(Player player)
 	{
-    	Logger.call(this,"SpawnPoint.meetPlayer(player)");
-    	Logger.ret(this,"SpawnPoint.meetPlayer(player)");
+    	Logger.call(this,"meetPlayer(player)");
+    	Logger.ret(this,"meetPlayer(player)");
 	}
 }
