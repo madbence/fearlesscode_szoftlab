40553f600a655ac14a8a102741453c8d66c4a729
Bence Danyi <madbence@gmail.com>
2012-03-18 22:06:06 +0100

    EntityContainer fix

--- src/fearlesscode/EntityContainer.java
+++ src/fearlesscode/EntityContainer.java
@@ -1,15 +1,12 @@
-/**
- * Created by IntelliJ IDEA.
- * User: tibikolozsi
- * Date: 3/16/12
- * Time: 12:48 PM
- * To change this template use File | Settings | File Templates.
- */
-public class EntityContainer {
+package fearlesscode;
+
+public class EntityContainer
+{
     Entity entity;
     EntityPosition position;
     
-    EntityContainer(Entity en, EntityPosition pos){
+    EntityContainer(Entity en, EntityPosition pos)
+    {
         entity = en;
         position=pos;
     }
