17d3e9b497417147080ed86aadbe4e92d057a17c
Bence Danyi <madbence@gmail.com>
2012-03-19 00:32:20 +0100

    Szkeleton implementacio

--- src/fearlesscode/Szkeleton.java
+++ src/fearlesscode/Szkeleton.java
@@ -4,6 +4,20 @@ class Szkeleton
 {
 	public static void Main(String[] args)
 	{
-		
+		Game game=new Game();
+		//inicializalas
+		game.start();
+
+		//blokk mozgatas
+		game.getPlayField().move(null, 1);
+
+		//jatekos mozgatas
+		game.getPlayField().getPlayer().move(null);
+
+		//tick
+		game.getPlayField().tick();
+
+		//toggleMode
+		game.getPlayField().toggleMode();
 	}
 }
