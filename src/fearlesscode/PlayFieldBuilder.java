f9ecf85e4327b9785bac20e6625206c0c6d6f5d0
Tibi Kolozsi <tibi.kolozsi@gmail.com>
2012-03-19 11:24:14 +0100

    PlayFieldBuilder FIX2

--- src/fearlesscode/PlayFieldBuilder.java
+++ src/fearlesscode/PlayFieldBuilder.java
@@ -38,121 +38,29 @@ public class PlayFieldBuilder
         	}
         	if(i>4){
         		blocks.get(i).setNeighbour(blocks.get(i-4),0,true);
-        	}
-        	
-            Wall wall = new Wall(playField);
-            Door door = new Door(0, playField);
-            Key  key = new Key(playField);
-            
-            blocks.get(0).addEntity(null,wall);
-            blocks.get(0).addEntity(null,door);
-            blocks.get(0).addEntity(null,key);
-        	
-        		
+        	}		
         }
         
-        /*ArrayList<Block> blocks = new ArrayList<Block>();
-        
-        PlayField playField = new PlayField();
-        Block blockToBuildUp = new FilledBlock();
-        
-        Player player = new Player();
         Wall wall = new Wall(playField);
         Door door = new Door(0, playField);
         Key  key = new Key(playField);
         
-        blockToBuildUp.addEntity(null, wall);
-        blockToBuildUp.addEntity(null, key);
-        blockToBuildUp.addEntity(null, wall);
-        blockToBuildUp.addEntity(null, door);
+        blocks.get(0).addEntity(null,wall);
+        blocks.get(0).addEntity(null,door);
+        blocks.get(0).addEntity(null,key);
         
-        
-        playField.addPlayer(null, player);
-        player.enterBlock(blockToBuildUp);
-        
-        blocks.add(blockToBuildUp);
-        
-        for(int i =1; i<numberOfBlocks; i++)
-        {
-        	if(i<numberOfBlocks-1){
-        		blocks.add(new FilledBlock());
-        	}
-        	else{
-        		blocks.add(new EmptyBlock());
-        	}
-        }
-        
-        for(int i = 0; i < dimension; i++)
-        {
-            try
-            {
-                blocks.get(i).setNeighbour(blocks.get(i+1), 1, true);
-                blocks.get(i).setNeighbour(blocks.get(i-1), 3, true);
-                blocks.get(i).setNeighbour(blocks.get(i+4), 2, true);
-                blocks.get(i).setNeighbour(blocks.get(i-4), 0, true);
-            }
-            catch(Exception e)
-            {
-            
-            }
-        }
-        
-        
-        
-        */
-        /*for(int i = 0; i < numberOfBlocks; i++)
-        {
-           
-            Block block;
-            
-            
-            if(i == numberOfEmptyBlock)
-            {
-            	block = new EmptyBlock();
-                block.addBlock(block);
-            }
-            else
-            {
-            	block=new FilledBlock();
-            	blocks.addBlock(block);
-            }
-            
-            playField.addBlock(new Position(), block);
-        }
-        
-        
-        for(i = 0; i < dimension; i++)
-        {
-            try
-            {
-                blocks.get(i).setNeighbour(blocks.get(i+1), 1, true);
-                blocks.get(i).setNeighbour(blocks.get(i-1), 3, true);
-                blocks.get(i).setNeighbour(blocks.get(i+4), 2, true);
-                blocks.get(i).setNeighbour(blocks.get(i-4), 0, true);
-            }
-            catch(Exception e)
-            {
-            
-            }
+        for(int i=0; i<numberOfBlocks; i++){
+        	pf.addBlock(null,blocks.get(i));
         }
         
         Player player = new Player();
-        Wall wall = new Wall(playField);
-        Door door = new Door(0, playField);
-        Key  key = new Key(playField);
         
-        blockToBuildUp.addEntity(null, wall);
-        blockToBuildUp.addEntity(null, key);
-        blockToBuildUp.addEntity(null, wall);
-        blockToBuildUp.addEntity(null, door);
-        
-
-        playField.setPlayer(player,null);
-        player.enterBlock(blockToBuildUp);
-
-        
+        blocks.get(0).setPlayer(player);
+        pf.setPlayer(player);
+        player.enterBlock(blocks.get(0));
+       
         Logger.ret("PlayFieldBuilder", "createPlayField()");
-        return null;
+        return pf;
 	}
 
 }
