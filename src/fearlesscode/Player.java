67e0e063152cf5a59f162861da4bc69576a87e0b
Bence Danyi <madbence@gmail.com>
2012-03-19 10:53:44 +0100

    Player implementáció

--- src/fearlesscode/Player.java
+++ src/fearlesscode/Player.java
@@ -2,70 +2,70 @@ package fearlesscode;
 
 import fearlesscode.util.*;
 
-public class Player {
+public class Player
+{
+	private Speed speed;
+	private ArrayList<Block> activeBlocks;
 
-	private int obtainedKeys;
-	private int speed;
-	private Block activeBlocks;
-
-	public Player(){
-            Logger.reg(this, "addKey()");
+	public Player()
+	{
+		Logger.reg(this, "player");
+		activeBlocks=new ArrayList<Block>();
 	}
 
 
-	public void addKey(){
-            Logger.call(this, "addKey()");
-            
-            obtainedKeys++;
-            
-            Logger.ret(this, "addKey()");
-	}
-
-	/**
-	 * 
-	 * @param block
-	 */
-	public void enterBlock(Block block){
-            Logger.call(this, "enterBlock(Block)");
-            
-            activeBlocks = block;
-            
-            Logger.ret(this, "addKey(Block)");
-	
-	}
-
-	public void getActiveBlocks(){
-            Logger.call(this, "getActiveBlocks()");
-            
-            Logger.ret(this, "getActiveBlocks()");
-	
-	}
-
-	public int getObtainedKeys(){
-            Logger.call(this, "getObtainedKeys()");
-            
-            Logger.ret(this, "getObtainedKeys()");
-            return 0;
+	public void addKey()
+	{
+		Logger.call(this, "addKey()");
+		Logger.ret(this, "addKey()");
 	}
 
 	/**
 	 * 
 	 * @param block
 	 */
-	public void leaveBlock(Block block){
-            Logger.call(this, "leaveBlock(Block)");
-            
-            Logger.ret(this, "leaveBlock(Block)");
+	public void enterBlock(Block block)
+	{
+		Logger.call(this, "enterBlock(Block)");
+		activeBlocks.add(block);
+		Logger.ret(this, "addKey(Block)");
+
+	}
+
+	public ArrayList<Block> getActiveBlocks()
+	{
+		Logger.call(this, "getActiveBlocks()");
+		Logger.ret(this, "getActiveBlocks()");
+		return activeBlocks;
+	}
+
+	public int getObtainedKeys()
+	{
+		Logger.call(this, "getObtainedKeys()");
+		Logger.ret(this, "getObtainedKeys()");
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param block
+	 */
+	public void leaveBlock(Block block)
+	{
+		Logger.call(this, "leaveBlock(Block)");
+		activeBlocks.remove(activeBlocks.indexOf(block));
+		Logger.ret(this, "leaveBlock(Block)");
 	}
 
 	/**
 	 * 
 	 * @param dir
 	 */
-	public void move(Direction dir){
-            Logger.call(this, "move(Direction)");
-            
-            Logger.ret(this, "move(Direction)");
+	public void move(Speed newSpeed)
+	{
+		Logger.call(this, "move(dir)");
+
+		Logger.ret(this, "move(dir)");
 	}
 
 }
