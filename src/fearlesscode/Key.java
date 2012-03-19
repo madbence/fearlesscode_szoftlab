7640bfbe43bd54f343445a55581043587a7ed504
Bence Danyi <madbence@gmail.com>
2012-03-19 13:17:38 +0100

    Key kommentelve

--- src/fearlesscode/Key.java
+++ src/fearlesscode/Key.java
@@ -4,7 +4,9 @@ import fearlesscode.util.*;
 
 public class Key extends Entity
 {
-
+	/**
+	 * Létrehoz egy kulcs objektumot, nem felvett állapottal
+	 */
 	public Key(PlayField playField)
 	{
 		//Logger.call(this,"Key.init()");
@@ -13,6 +15,11 @@ public class Key extends Entity
 		//Logger.ret(this,"Key.init()");
 	}
 
+	/**
+	 * Ha a játékos ütközik egy kulcs objektummal, akkor a megszerzettsége állapotától
+	 * függően megnöveli a játékos kulcsainak a számát
+	 * @param Player player A játékos, akivel a kulcs interakcióban nem
+	 */
     public void meetPlayer(Player player)
     {
     	Logger.call(this,"meetPlayer(player)");
