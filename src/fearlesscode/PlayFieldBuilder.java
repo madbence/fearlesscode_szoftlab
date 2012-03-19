d151f31755ffd4ba6623e539276a85be52b4f929
Bence Danyi <madbence@gmail.com>
2012-03-19 09:58:12 +0100

    playFieldbuilder fix

--- src/fearlesscode/PlayFieldBuilder.java
+++ src/fearlesscode/PlayFieldBuilder.java
@@ -8,62 +8,69 @@ public class PlayFieldBuilder {
 	}
 
 
-	public static PlayField createPlayField(){
-            Logger.reg(this, "createPlayField()");
+	public static PlayField createPlayField()
+    {
+        //Logger.reg(this, "createPlayField()");
+        
+        int numberOfBlocks = 16;
+        int numberOfEmptyBlock = 0;
+        int numberOfBlockToBuildUp = 1;
+        int dimension = 4;
+        
+        ArrayList<Block> blocks = new ArrayList<Block>();
+        
+        PlayField playField = new PlayField();
+        Block blockToBuildUp;
+        
+        for(int i = 0; i < numberOfBlocks; i++)
+        {
+            Block block;
+            blocks.add(block);
             
-            int numberOfBlocks = 16;
-            int numberOfEmptyBlock = 0;
-            int numberOfBlockToBuildUp = 1;
-            int dimension = 4;
-            
-            ArrayList<Block> blocks = new ArrayList<Block>;
-            
-            PlayField playField = new PlayField();
-            Block blockToBuildUp;
-            
-            for(int i = 0; i < numberOfBlocks; i++){
-                Block block;
-                blocks.add(block);
-                
-                if(i == numberOfEmptyBlock){
-                    block = new EmptyBlock();
-                } else {
-                    block = new Block();
-                }
-                
-                if(i == numberOfBlockToBuildUp){
-                    blockToBuildUp = block;
-                }
-               
-                playField.addBlock(new Position(), eBlock);
+            if(i == numberOfEmptyBlock)
+            {
+                block = new EmptyBlock();
             }
-            
-            for(i = 0; i < dimension; i++){
-                try{
-                    blocks.get(i).setNeighbour(blocks.get(i+1), 1, true);
-                    blocks.get(i).setNeighbour(blocks.get(i-1), 3, true);
-                    blocks.get(i).setNeighbour(blocks.get(i+4), 2, true);
-                    blocks.get(i).setNeighbour(blocks.get(i-4), 0, true);
-                } catch {
-                
-                }
+            else
+            {
+                block = new Block();
             }
+            if(i == numberOfBlockToBuildUp)
+            {
+                blockToBuildUp = block;
+            }
+            playField.addBlock(new Position(), eBlock);
+        }
+        
+        for(i = 0; i < dimension; i++)
+        {
+            try{
+                blocks.get(i).setNeighbour(blocks.get(i+1), 1, true);
+                blocks.get(i).setNeighbour(blocks.get(i-1), 3, true);
+                blocks.get(i).setNeighbour(blocks.get(i+4), 2, true);
+                blocks.get(i).setNeighbour(blocks.get(i-4), 0, true);
+            }
+            catch(Exception e)
+            {
             
-            Player player = new Player();
-            Wall wall = new Wall();
-            Door door = new Door();
-            Key  = new Key();
-            
-            blockToBuildUp.addEntity(new EntityPosition, wall);
-            blockToBuildUp.addEntity(new EntityPosition, key);
-            blockToBuildUp.addEntity(new EntityPosition, wall);
-            blockToBuildUp.addEntity(new EntityPosition, door);
-            
-            playerField.addPlayer(new EntityPosition, player);
-            player.enterBlock(blockToBuildUp);
-            
-            Logger.reg(this, "createPlayField()");
-            return null;
+            }
+        }
+        
+        Player player = new Player();
+        Wall wall = new Wall();
+        Door door = new Door();
+        Key  = new Key();
+        
+        blockToBuildUp.addEntity(null, wall);
+        blockToBuildUp.addEntity(null, key);
+        blockToBuildUp.addEntity(null, wall);
+        blockToBuildUp.addEntity(null, door);
+        
+        playerField.addPlayer(null, player);
+        player.enterBlock(blockToBuildUp);
+        
+        //Logger.reg(this, "createPlayField()");
+        return null;
 	}
 
 }
