15909542e05284eb99f7297e623089199d482a59
Bence Danyi <madbence@gmail.com>
2012-03-20 17:27:55 +0100

    BlockContainer hozzaadva

---
+++ src/fearlesscode/BlockContainer.java
@@ -0,0 +1,12 @@
+package fearlesscode;
+
+public class BlockContainer
+{
+	public Position position;
+	public Block block;
+	public BlockContainer(Position p, Block b)
+	{
+		position=p;
+		block=b;
+	}
+}
