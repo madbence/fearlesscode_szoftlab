306591f2f8415939be02549d519b1b7008745380
Bence Danyi <madbence@gmail.com>
2012-03-20 17:52:50 +0100

    Key implementacio

--- src/fearlesscode/Key.java
+++ src/fearlesscode/Key.java
@@ -4,29 +4,35 @@ import fearlesscode.util.*;
 
 public class Key extends Entity
 {
+	private static int count=0;
+	private boolean isObtained;
 	/**
 	 * Létrehoz egy kulcs objektumot, nem felvett állapottal
+	 * @param playField Az aktuális pálya referenciája
 	 */
 	public Key(PlayField playField)
 	{
-		//Logger.call(this,"Key.init()");
 		super(playField);
-		Logger.reg(this, "key");
-		//Logger.ret(this,"Key.init()");
+		String name="key"+(count++);
+		Logger.reg(this, name);
+		Logger.call(this,"init()");
+		isObtained=false;
+		Logger.ret(this,"init()");
 	}
 
 	/**
 	 * Ha a játékos ütközik egy kulcs objektummal, akkor a megszerzettsége állapotától
 	 * függően megnöveli a játékos kulcsainak a számát
-	 * @param Player player A játékos, akivel a kulcs interakcióban nem
+	 * @param player A játékos, akivel a kulcs interakcióban nem
 	 */
     public void meetPlayer(Player player)
     {
     	Logger.call(this,"meetPlayer(player)");
-    	if(!Logger.ask("Meg lett mar szerezve a kulcs?"))
+    	if(!isObtained)
 		{
 			player.addKey();
 			playField.setSpawnPosition(this);
+			isObtained=true;
 		}
     	Logger.ret(this,"meetPlayer(player)");
 	}
