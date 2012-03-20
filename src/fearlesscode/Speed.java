8b96114e2146329c11c40c108f6b7b636042aaa3
Bence Danyi <madbence@gmail.com>
2012-03-20 20:04:43 +0100

    Speed metodusok hozzaadva

--- src/fearlesscode/Speed.java
+++ src/fearlesscode/Speed.java
@@ -1,6 +1,23 @@
 
 package fearlesscode;
 
-public class Speed {
+public class Speed
+{
+	private double x;
+	private double y;
+	public Speed(double x, double y)
+	{
+		this.x=x;
+		this.y=y;
+	}
 
+	public double getX()
+	{
+		return x;
+	}
+
+	public double getY()
+	{
+		return y;
+	}
 }
