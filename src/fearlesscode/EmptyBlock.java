f97e3957748173f7103e8c635e3399af061e61d8
Bence Danyi <madbence@gmail.com>
2012-03-26 20:59:24 +0200

    Kommentelés karakterkódolás javítva Formázás javítva Logolás kiszedve

--- src/fearlesscode/EmptyBlock.java
+++ src/fearlesscode/EmptyBlock.java
@@ -2,10 +2,9 @@ package fearlesscode;
 
 import fearlesscode.util.*;
 
-
 /**
-* Az üres blokkot reprezentálja, gyakorlatilag a Block osztály default (üres)
-* implemetáció- ja.
+* Az üres blokkot reprezentálja, gyakorlatilag a Block osztály default (ü̈res)
+* implemetációja.
 */
 public class EmptyBlock extends Block
 {
@@ -16,25 +15,21 @@ public class EmptyBlock extends Block
 	public EmptyBlock(PlayField pf)
 	{
 		super(pf);
-		Logger.call(this,"init()");
-		Logger.ret(this,"Block.init()");
 	}
 	
 	/**
-	* Mivel üres blokk, semmi sincs benne, így ez a metódus nem csinál semmit.
+	* Mivel üres blokk, semmi sincs benne, így ez a metódus nem csinál semmit.
 	*/
 	public void checkBorders()
 	{
-		Logger.call(this,"checkBorders()");
-		Logger.ret(this,"checkBorders()");
+
 	}
 	
 	/**
-	* Mivel üres blokk, semmi sincs benne, így ez a metódus nem csinál semmit.
+	* Mivel ü̈res blokk, semmi sincs benne, így ez a metódus nem csinál semmit.
 	*/
 	public void processCollisions()
 	{
-		Logger.call(this,"processCollisions()");
-		Logger.ret(this,"processCollisions()");
+
 	}
 }
