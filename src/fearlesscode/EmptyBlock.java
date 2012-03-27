0cbb86f103c654e09d52644cb8c1b8b9ee3694aa
Bence Danyi <madbence@gmail.com>
2012-03-27 17:28:26 +0200

    Kommenteles javitva

--- src/fearlesscode/EmptyBlock.java
+++ src/fearlesscode/EmptyBlock.java
@@ -3,31 +3,31 @@ package fearlesscode;
 import fearlesscode.util.*;
 
 /**
-* Az üres blokkot reprezentálja, gyakorlatilag a Block osztály default (ü̈res)
-* implemetációja.
-*/
+ * Az üres blokkot reprezentálja, gyakorlatilag a Block osztály default (ü̈res)
+ * implemetációja.
+ */
 public class EmptyBlock extends Block
 {
 
 	/**
-	* Az EmptyBlock konstruktora, PlayField referenciával.
-	*/
+	 * Az EmptyBlock konstruktora, PlayField referenciával.
+	 */
 	public EmptyBlock(PlayField pf)
 	{
 		super(pf);
 	}
 	
 	/**
-	* Mivel üres blokk, semmi sincs benne, így ez a metódus nem csinál semmit.
-	*/
+	 * Mivel üres blokk, semmi sincs benne, így ez a metódus nem csinál semmit.
+	 */
 	public void checkBorders()
 	{
 
 	}
 	
 	/**
-	* Mivel ü̈res blokk, semmi sincs benne, így ez a metódus nem csinál semmit.
-	*/
+	 * Mivel ü̈res blokk, semmi sincs benne, így ez a metódus nem csinál semmit.
+	 */
 	public void processCollisions()
 	{
 
