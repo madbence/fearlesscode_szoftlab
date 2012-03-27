eccdd095872a3ca36e26d09860eaac64e4063dd7
Bence Danyi <madbence@gmail.com>
2012-03-27 17:47:00 +0200

    Logolás kiszedése

--- src/fearlesscode/SpawnPoint.java
+++ src/fearlesscode/SpawnPoint.java
@@ -6,13 +6,10 @@ public class SpawnPoint extends Entity
 {
 	public SpawnPoint(PlayField playField)
 	{
-		//Logger.call(this,"SpawnPoint.init()");
 		super(playField);
-		//Logger.ret(this,"SpawnPoint.init()");
 	}
 	public void meetPlayer(Player player)
 	{
-    	Logger.call(this,"meetPlayer(player)");
-    	Logger.ret(this,"meetPlayer(player)");
+		
 	}
 }
