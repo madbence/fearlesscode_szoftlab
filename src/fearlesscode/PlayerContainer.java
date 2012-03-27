37b79bca2aa5e7284054017c7a09506b77971a51
Bence Danyi <madbence@gmail.com>
2012-03-27 17:44:51 +0200

    Kommentelés javítva

--- src/fearlesscode/PlayerContainer.java
+++ src/fearlesscode/PlayerContainer.java
@@ -2,22 +2,20 @@ package fearlesscode;
 
 
 /**
-*	A Player-t és a Player pozicióját tároló segédosztály.
-*/
-
+ * A Player-t és a Player pozicióját tároló segédosztály.
+ */
 public class PlayerContainer
 {
-    Player player;
-    EntityPosition position;
-    
-    
-    /**
-    *	A PlayerContainer konstruktora, beállítja a Player referenciáját,
-    *	és pozicióját
-    */
-    PlayerContainer(Player player, EntityPosition pos)
-    {
-        this.player=player;
-        this.position=pos;
-    }
+	Player player;
+	EntityPosition position;
+
+	/**
+	 * A PlayerContainer konstruktora, beállítja a Player referenciáját,
+	 * és pozicióját.
+	 */
+	PlayerContainer(Player player, EntityPosition pos)
+	{
+		this.player=player;
+		this.position=pos;
+	}
 }
