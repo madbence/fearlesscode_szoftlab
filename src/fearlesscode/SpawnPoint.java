f271979de1717d7bd002b7c3b11bd9ef54db819e
Bence Danyi <madbence@gmail.com>
2012-03-29 16:37:05 +0200

    SpawnPoint kommentelve.
    Az implementáció véglegesnek tekinthető, mivel ez az osztály kvázi az Entity adapter osztálya.

--- src/fearlesscode/SpawnPoint.java
+++ src/fearlesscode/SpawnPoint.java
@@ -2,12 +2,24 @@ package fearlesscode;
 
 import fearlesscode.util.*;
 
+/**
+ * Az Entity osztály üres implementációja, a játékosok alapértelmezetten ennek az osztály egy példányának a helyén jönnek létre.
+ */
 public class SpawnPoint extends Entity
 {
+	/**
+	 * A SpawPoint konstruktora. Mivel üres implementáció, csak a szülő osztály konstruktorát hívja meg.
+	 * @param playField Az Entity konstruktorának továbbadott PlayField objektum.
+	 */
 	public SpawnPoint(PlayField playField)
 	{
 		super(playField);
 	}
+
+	/**
+	 * Ha a játékos egy SpawPointtal ütközik, nem történik semmi, így a metódus implementációja teljesen üres.
+	 * @param player A játékos, akivel ütközik az objektum.
+	 */
 	public void meetPlayer(Player player)
 	{
 		
