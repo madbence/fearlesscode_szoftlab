f8abcd8b1f03ca75d7982a4a22330b62d9e0cfa3
Tibi Kolozsi <tibi.kolozsi@gmail.com>
2012-04-05 21:11:39 +0200

    PlayFieldBuilder implementálása

--- src/fearlesscode/PlayFieldBuilder.java
+++ src/fearlesscode/PlayFieldBuilder.java
@@ -74,6 +74,83 @@ public class PlayFieldBuilder
 		//pf.setPlayer(player);						 //player beállítása a playfieldben
 		player.enterBlock(blocks.get(0));
 		return pf;*/
+		PlayField pf = new PlayField(game);
+		ArrayList<Block> blocks = new ArrayList<Block>();
+		MapFromJson mfj = new MapFromJson(file);
+		
+		for(int i=0; i<mfj.getNumberOfBlocks();i++)
+		{
+			if(mfj.isFilledBlock(i))
+			{
+				blocks.add(new FilledBlock(pf));
+			}
+			else
+			{
+				blocks.add(new EmptyBlock(pf));
+			}
+		}
+		for(int i=0; i<mfj.getNumberOfBlocks(); i++)
+		{			 
+			if(i%4!=0)
+			{
+				blocks.get(i).setNeighbour(blocks.get(i-1),3,true);
+			}
+			if(i>4)
+			{
+				blocks.get(i).setNeighbour(blocks.get(i-4),0,true);
+			}		
+		}
+		for(int i=0; i<mfj.getNumberOfBlocks();i++)
+		{
+			if(mfj.isFilledBlock(i))								//ha FilledBlock-ról van szó
+			{
+				for(int j=0;j<mfj.getNumberOfEntitiesInBlock(j);j++)
+				{
+					EntityPosition entityPos = new EntityPosition(mfj.getXPositionOfEntityInBlock(i, j),mfj.getYPositionOfEntityInBlock(i, j));
+					if("Wall".equals(mfj.getTypeOfEntityInBlock(i, j)))
+					{
+						double width = mfj.getWidthOfWall(i, j);
+						double height = mfj.getHeightOfWall(i, j);
+						Wall wall = new Wall(pf,width,height);
+						blocks.get(i).addEntity(entityPos,wall);
+					}
+					else if("Door".equals(mfj.getTypeOfEntityInBlock(i,j)))
+					{
+						int keys = mfj.getNumberOfRequiredKeys(i, j);
+						Door door = new Door(keys,pf);
+						blocks.get(i).addEntity(entityPos,door);
+					}
+					else if("Key".equals(mfj.getTypeOfEntityInBlock(i,j)))
+					{
+						Key key = new Key(pf);
+						blocks.get(i).addEntity(entityPos, key);
+					}	
+				}
+			}
+		}
+		
+		for(int i=0; i<mfj.getNumberOfBlocks();i++)
+		{
+			Position pos = new Position(mfj.getXPositionOfBlock(i),mfj.getYPositionOfBlock(i));
+			pf.addBlock(pos, blocks.get(i));
+		}
+		
+		for(int i=0; i<mfj.getNumberOfBlocks();i++)
+		{
+			for(int j=0; i<mfj.getNumberOfPlayersInBlock(i);j++)
+			{
+				Player player = new Player();
+				double xSP = mfj.getXSpawnPointOfPlayerInBlock(i, j);
+				double ySP = mfj.getYSpawnPointOfPlayerInBlock(i, j);
+				SpawnPoint spawnPoint = new SpawnPoint(pf,xSP,ySP);
+				double xEP = mfj.getXPositionOfPlayerInBlock(i, j);
+				double yEP = mfj.getYPositionOfPlayerInBlock(i, j);
+				EntityPosition entityPos = new EntityPosition(xEP,yEP);
+				blocks.get(i).setPlayer(player, entityPos);
+				pf.addPlayer(player, spawnPoint);
+				player.enterBlock(blocks.get(i));
+			}
+		}
 		return null;
 	}
 
