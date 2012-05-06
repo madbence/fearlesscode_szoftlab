26f34b64fb067807bb0616f5f42c3f926502ce1b
Zsolt Dudas <me@zsoltdudas.com>
2012-05-06 15:48:09 +0200

    Added pictures of static screens.

--- compile.bat
+++ compile.bat
@@ -4,7 +4,9 @@ rm -r -f build
 mkdir build
 echo Compiling...
 javac -classpath .\src\ -encoding UTF-8 -d .\build\ .\src\fearlesscode\app\Grafikus.java
+
 echo Copy resources...
 xcopy .\src\fearlesscode\gui\images .\build\fearlesscode\gui\images\
 xcopy .\src\fearlesscode\gui\images\menus .\build\fearlesscode\gui\images\menus\
+
 @echo on
